8afec98385d1d3f4c4f6594edf9a60cc05e19722
bryan <bryancorado777.com>
2022-12-12 17:40:33 -0600

    first commit

---
+++ src/main/java/main.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 24DD018LA
+ */
+public class main {
+    public static void main(String [] args ){
+           formulario form = new formulario();
+           form.setVisible(true);
+        
+    } 
+}
